6304cf1cb7cc13599cd0cc25da047c319a64138f
emileC1 <emile.cornu@student.yncrea.Fr>
2022-10-21 11:11:25 +0200

    restart finish

--- src/main/java/game/Game.java
+++ src/main/java/game/Game.java
@@ -3,6 +3,7 @@ package game;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.Pane;
 
+import java.security.Key;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -32,6 +33,10 @@ public class Game {
             player.moveX(-5, level);
         }
 
+        if(controler.isPressed((KeyCode.R))){
+            restart();
+        }
+
         if (controler.isPressed(KeyCode.D) && player.playerNode.getTranslateX() + player.width <= level.width - 5) {
             player.moveX(5, level);
         }
@@ -45,8 +50,9 @@ public class Game {
     public void restart(){
 
         player.playerNode.setTranslateX(0);
-        player.playerNode.setTranslateY(600);
+        player.playerNode.setTranslateY(500);
         player.playerVelocity.add(-player.playerVelocity.getX(),-player.playerVelocity.getY());
+        gameRoot.setLayoutX(0);
     }
 
 }
