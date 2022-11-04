d897c21869cee07d64a941706a5fdde6cb65f770
loick-zirotti <loick.zirotti@student.junia.com>
2022-11-04 17:01:15 +0100

    Update RotatingSaw.java
    Ajout baton de trap

--- src/main/java/game/traps/RotatingSaw.java
+++ src/main/java/game/traps/RotatingSaw.java
@@ -5,12 +5,18 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import game.Game;
 import javafx.geometry.Point2D;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.transform.Rotate;
 
 
 public class RotatingSaw extends Trap{
     public float radius;
 
     public Point2D center;
+
+    public Rectangle rectangleSaw = new Rectangle();
+
+    public int compteur = 0;
 
     public RotatingSaw(float x, float y, Game game) {
 
@@ -21,10 +27,25 @@ public class RotatingSaw extends Trap{
         trap.setTranslateY(y - radius);
         trap.setFill(Color.DARKGREEN);
 
+        float length = (float) Math.sqrt(Math.pow(trap.getTranslateX()-center.getX(),2)+ Math.sqrt(Math.pow(trap.getTranslateY()-center.getY(),2)));
+
+        rectangleSaw.setWidth((length*1.5));
+        rectangleSaw.setHeight(game.level.platformHeight);
+
+        rectangleSaw.setX(trap.getTranslateX()- 0.5*rectangleSaw.getWidth());
+        rectangleSaw.setY(trap.getTranslateY() - 0.5*rectangleSaw.getWidth());
+        rectangleSaw.setFill(Color.PINK);
+
+
+
 
         this.node = trap;
 
         game.gameRoot.getChildren().add(trap);
+        game.gameRoot.getChildren().add(rectangleSaw);
+
+
+
     }
 
     public void updateTrap(Level level) {
@@ -33,7 +54,16 @@ public class RotatingSaw extends Trap{
             return;
         }
 
+        if(compteur <1) {
+            rectangleSaw.getTransforms().add(new Rotate(90, center.getX(), center.getY()));
+            compteur++;
+        }
+
+
         node.setTranslateX(center.getX() + radius * Math.cos(level.game.time / (float)60));
         node.setTranslateY(center.getY() + radius * Math.sin(level.game.time / (float)60));
+
+
+      rectangleSaw.getTransforms().add(new Rotate(0.955,center.getX(),center.getY()));
     }
 }
